81de1318f0ef8a1ab1e03154159b2abfd449fe63
per <per@02ab37ae-38fb-4317-a5bb-07071a9c5d0d>
2011-07-27 15:41:21 +0000

    Added php to framework
    git-svn-id: http://svn.meltsoftware.com/melt-website@5092 02ab37ae-38fb-4317-a5bb-07071a9c5d0d

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-<title>Melt Framework</title>
+<title>Melt PHP Framework</title>
 
 <link rel="stylesheet" href="/style.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="/buttons.css" type="text/css" media="screen" />
